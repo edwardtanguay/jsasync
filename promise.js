96543cbe4e7a0da4c862708b08c605edf7e65f2b
Edward Tanguay (wk.win) <edward.tanguay@gefegmbh.local>
2020-07-07 16:26:41 +0200

    promise

---
+++ promise.js
@@ -0,0 +1,15 @@
+const prom1 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('the result')
+	}, 1000);
+});
+
+prom1.then((value) => {
+	console.log(value);
+});
+
+console.log('promise: ', prom1);
+
+setTimeout(() => {
+	console.log('promise again: ', prom1);
+}, 1500);
